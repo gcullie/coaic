39f34658ea078b3b3782670f6a7bf607b6940723
Cullen <gregcullie@gmail.com>
2017-09-11 09:55:03 -0500

    Updated for Google Cloud
    Can be installed on Google Cloud Ubuntu 16.04.LTS Image

--- training-setup.sh
+++ training-setup.sh
@@ -8,20 +8,35 @@ NUMBEROFWORKER=4
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable
 sudo apt-get update
 sudo apt-get install -y tmux htop cmake
-apt-get upgrade
+sudo apt-get upgrade
 sudo apt-get install golang libjpeg-turbo8-dev make
-sudo pip install universe
-pip install six
-sudo pip install opencv-python
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo apt-get install libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libxxf86vm-dev libgl1-mesa-dev mesa-common-dev
+sudo pip install scipy
 
 cd ~
-git clone https://github.com/openai/universe.git
-cd universe
-pip install -e .
-sudo apt-get update
+git clone https://github.com/openai/go-vncdriver.git
+cd go-vncdriver
+sudo python build.py
+sudo pip install -e .
+cd ~
+
+sudo pip install universe
+sudo pip install six
+sudo pip install opencv-python
+
+#cd ~
+#git clone https://github.com/openai/universe.git
+#cd universe
+#pip install -e .
+#sudo apt-get update
 
 sudo pip install Twisted==16.4.1
-sudo pip install opencv-python, htop
+sudo pip install opencv-python
+sudo apt-get -y install htop ncdu tmux tree
+sudo apt-get install iotop sysstat dbench dtrx mc slurm qalc
+sudo apt-get install zlib1g-dev
 
 cd ~
 git clone https://github.com/openai/universe-starter-agent.git
