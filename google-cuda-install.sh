b7ec085be20d174bee087d2e8a7105ea00387a8c
Cullen <gregcullie@gmail.com>
2018-10-11 13:47:25 -0500

    Update google-cuda-install.sh

--- google-cuda-install.sh
+++ google-cuda-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Checking for CUDA and installing ref. https://cloud.google.com/compute/docs/gpus/add-gpus#install-driver-script"
 # Check for CUDA and try to install.
+#must be run with sudo
 if ! dpkg-query -W cuda-8-0; then
   # The 16.04 installer works with 16.10.
   curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
